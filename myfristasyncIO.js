2b8c16a5ec03b22084e0866a7baf739337d6a4c7
alejandro <alejandrocameselle@gmail.com>
2016-10-03 08:52:48 +0200

    mi primera entrada/salida

---
+++ myfristasyncIO.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+
+fs.readFile(process.argv[2],
+
+	function callback(error, buffer) { /* ... */
+
+		if (error) {
+			console.log(error)
+		}else{
+
+
+		var string = buffer.toString();
+
+		var lineas = string.split('\n');
+
+
+		console.log(lineas.length - 1);
+
+		function callback(error, data) { /* ... */ }
+		}
+	})
